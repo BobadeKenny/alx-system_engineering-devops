8d2f6ba110c72de80f15e4ea1ac9854e281a74e5
Kehinde Bobade <kennygbeminiyi@gmail.com>
2022-11-02 05:35:22 +0100

    done

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""function that queries the Reddit API
+and returns the number of subscribers"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """function that queries the Reddit API
+    and returns the number of subscribers"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+        "User-Agent": "alx: BobadeKenny"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    results = response.json().get("data")
+    return results.get("subscribers")
